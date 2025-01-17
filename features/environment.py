3c3901d19cc86ef6f5e869b02025c2932a519e5e
TomaszKarski <tomasz.karski@miinto.pl>
2025-01-17 19:53:36 +0100

    restart driver before each scenario

--- features/environment.py
+++ features/environment.py
@@ -1,34 +1,12 @@
-import threading
-
 from behave.model import Scenario
-from behave.runner import Context
 from model.context_data import AdditionalContext, Pages
 from model.webdriver.chrome_driver import Driver
-from retry import retry
 
 
-@retry(TimeoutError, tries=3)
-def process_clearing_browser_data(browser: Driver):
-    thread = threading.Thread(
-        target=browser.execute, args=(
-            f'SEND_COMMAND_{browser.session_id}',
-            dict(cmd='Storage.clearDataForOrigin', params={"origin": '*', "storageTypes": 'all'})
-        )
-    )
-    thread.start()
-    thread.join(timeout=6)
-    if thread.is_alive():
-        raise TimeoutError('The browser data was not properly cleared')
-
-
-def before_all(context: Context):
+def before_scenario(context: AdditionalContext, _: Scenario):
     context.driver = Driver()
     context.pages = Pages(context.driver)
 
 
-def before_scenario(context: AdditionalContext, scenario: Scenario):
-    process_clearing_browser_data(context.driver)
-
-
-def after_all(context: AdditionalContext):
+def after_scenario(context: AdditionalContext, _: Scenario):
     context.driver.quit()
